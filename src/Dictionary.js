252fa2760f4d6ad3a6e1aed3a9f9c6e8664aad5b
Rhitree <rhian.jenkins90@gmail.com>
2026-01-22 17:52:38 +0000

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -20,7 +20,7 @@ export default function Dictionary(props) {
     }
 
     function search() {
-        let apiKey = `b3a37c1584b0oatf80a196c74f3071cb`;
+        let apiKey = "b3a37c1584b0oatf80a196c74f3071cb";
         let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
         axios.get(apiUrl).then(handleResponse);
 
