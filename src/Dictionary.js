2d1abfc416be1d9e2fd6011c46ec72a8485964a5
Rhitree <rhian.jenkins90@gmail.com>
2026-01-22 17:06:24 +0000

    Trying to fix bug, results text has dissapered on Search but Photo is showing

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./Dictionary.css";
-import Results from "./Results";
 import Photos from "./Photos";
+import Results from "./Results";
+import "./Dictionary.css";
+
 
 export default function Dictionary(props) {
     let [keyword, setKeyword]  = useState(props.defaultKeyword);
@@ -10,7 +11,7 @@ export default function Dictionary(props) {
     let [loaded, setLoaded] = useState(false);
     let [photos, setPhotos] = useState([]);
 
-    function handleDictionaryResponse(response) {
+    function handleResponse(response) {
         setResults(response.data[0]);
     }
 
@@ -19,48 +20,48 @@ export default function Dictionary(props) {
     }
 
     function search() {
-        let apiKey = "b3a37c1584b0oatf80a196c74f3071cb"
+        let apiKey = `b3a37c1584b0oatf80a196c74f3071cb`
         let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`
-        axios.get(apiUrl).then(handleDictionaryResponse);
+        axios.get(apiUrl).then(handleResponse);
 
         let pexelsApiKey ="ozZf2v8mtOfAXNlDtC6vD6f5rv51XfK7OTGJyuCGXNvy9NGyu3nnbnA8";
         let pexelsApiUrl=`https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
         let headers = { Authorization: `${pexelsApiKey}` };
-        axios.get(pexelsApiUrl, {headers: headers}).then(handlePexelsResponse)
-};
-
-function handleSubmit(event) {
-    event.preventDefault();
-    search()
-}
-
-function handleKeywordChange(event) {
-    setKeyword(event.target.value);
-}
-
-function load() {
-    setLoaded(true);
-    search();
-}
-
-if (loaded) {
-    return (
-        <div className="Dictionary">
-            <section>
-                <form onSubmit={handleSubmit}>
-                    <input type="search"
-                    autoFocus={true} 
-                    onChange={handleKeywordChange}
-                    defaultValue={props.defaultKeyword} 
-                    />
-                </form>
-            </section>
-            <Results results={results} />
-            <Photos photos={photos} />
-        </div>
-    );
-    } else {
-        load();
-        return "Loading";
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
-}
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        search()
+    }
+
+    function handleKeywordChange(event) {
+        setKeyword(event.target.value);
+    }
+
+    function load() {
+        setLoaded(true);
+        search();
+    }
+
+    if (loaded) {
+        return (
+            <div className="Dictionary">
+                <section>
+                    <form onSubmit={handleSubmit}>
+                        <input type="search"
+                        autoFocus={true} 
+                        onChange={handleKeywordChange}
+                        defaultValue={props.defaultKeyword} 
+                        />
+                    </form>
+                </section>
+                <Results results={results} />
+                <Photos photos={photos} />
+            </div>
+        );
+        } else {
+            load();
+            return "Loading";
+        }
+    }
